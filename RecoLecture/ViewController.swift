ce5b8addde839273446393ae6eb9e44196081d5c
Kevin Dixson <kdixson538@gmail.com>
2016-11-30 23:04:52 -0500

    Xcode bringup

---
+++ RecoLecture/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  RecoLecture
+//
+//  Created by Kevin on 11/30/16.
+//  Copyright © 2016 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var myClasses: UIButton!
+    @IBOutlet var upload: UIButton!
+    @IBOutlet var record: UIButton!
+    
+    @IBAction func onMyClassesClicked(_ sender: Any) {
+        //Nothing
+    }
+    
+    @IBAction func onUploadClicked(_ sender: Any) {
+        //Nothing
+    }
+    
+    @IBAction func onRecordClicked(_ sender: Any) {
+        //Nothing
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
